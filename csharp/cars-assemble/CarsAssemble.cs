f47227cfc2d1266dbf1577b3a37dcf2fef232795
Dan <danielfr500@gmail.com>
2022-04-29 22:56:27 -0400

    C# conditionals

---
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,36 @@
+using System;
+
+static class AssemblyLine
+{
+    public static double SuccessRate(int speed)
+    {
+
+        if(speed == 0)
+            {
+                return 0;
+            }
+        else if(speed >=0 && speed <=4)
+            {
+                return 1.0;
+            }
+        else if (speed >= 5 && speed <=8)
+            {
+                return .9;
+            }
+        else if (speed == 9)
+            {
+                return .8;
+            }
+        else 
+            {
+                return .77;
+            }
+        
+    }
+    
+    public static double ProductionRatePerHour(int speed) =>   SuccessRate(speed)*221*speed;
+
+   
+
+    public static int WorkingItemsPerMinute(int speed) => (int)ProductionRatePerHour(speed)/60;
+}
