24837564cbc9742840581af892842712f8953a15
Dan <danielfr500@gmail.com>
2022-08-08 21:51:34 -0400

    random Random.Next, Random.NextDouble

---
+++ csharp/roll-the-die/RollTheDie.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Player
+{
+
+    Random rnd = new Random();
+
+    public int RollDie()
+    {
+        return rnd.Next(1, 18);
+    }
+
+    public double GenerateSpellStrength()
+    {
+        return rnd.NextDouble()*100;
+    }
+}
