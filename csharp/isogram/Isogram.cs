df3c052ba45637c4e1b84c40d0bea4df53e6003f
Dan <danielfr500@gmail.com>
2022-10-14 23:02:59 -0400

    str.replace(), .ToLower(), str.ToCharArray(), new HashSet<char>()

---
+++ csharp/isogram/Isogram.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        word = word.Replace(" ","");                          // removes spaces
+        word = word.Replace("-","");                          // removes -
+        word = word.ToLower();                                // converts all to lowercase
+        char[] chArray = word.ToCharArray();                  // converts string to char array
+        HashSet<char> arraySet = new HashSet<char>(chArray);  // converts to set
+
+
+        //if the length/count of the set and the string are the same then it is an isogram
+        // if they are different than some letter is duplicated.
+
+        
+        if (arraySet.Count == word.Length){
+            return true;
+        } 
+        else{
+            return false;
+        }
+    }
+}
